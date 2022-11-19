e8af049a4d706c27184edae0782fe985640eaa93
murat <mrtyzt66@hotmail.com>
2022-11-19 22:33:41 +0300

    text

---
+++ python hw-b.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 14:01:07 2022
+
+@author: mrtyz
+"""
+
+for x in range(10):
+    for y in range(10):
+        if y>x:
+            print(x,y, sep='')
+           
