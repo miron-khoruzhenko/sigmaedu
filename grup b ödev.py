d8ddca5c057c2a0fc41747e6f92b6af4249f3139
nuriye22 <117525543+nuriye22@users.noreply.github.com>
2022-11-19 22:11:44 +0300

    Add files via upload

---
+++ grup b ödev.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 03:17:18 2022
+
+@author: NURİYE KARADAŞ
+"""
+
+
+for a in range(10):
+    for b in range(10):
+        if a==0:
+            print(a,b, end=", ")
+        elif a<b:
+            print(a,b, end=", ")
+
+
+
+    
