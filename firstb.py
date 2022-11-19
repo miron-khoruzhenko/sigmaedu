a28c5bf8db55669047dd5737b3d99c690ae3f938
cank641 <kayalican641@gmail.com>
2022-11-19 18:09:16 +0300

    efesmalt

--- firstb.py
+++ firstb.py
@@ -1,12 +1,12 @@
 for i in range(0, 101):
     if len(str(i)) == 1:
         i = "0"+str(i)
-    if (str(i)[0]) < (str(i)[1]):
+    if (str(i))[0] < (str(i))[1]:
         if i == 89:
             print(i)
-        print(i,"")
+            break
+        print(i,end=", ")
+    
 
 
-
-a = 200
 
