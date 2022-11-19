74a786a2599ff63d33ea1c1b595f28ebb3190e8e
Yigit <yigitozcan2001@hotmail.com>
2022-11-19 23:38:22 +0300

    Final

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,4 +1,11 @@
 
 for i in range(0,10):
- for j in range(0,10):
-  print(f"{i}{j}, ",end="")
+   for j in range(0,10):
+       if(j > i):
+          if(i == 9 & j == 9):
+             print(f'{i}{j}')
+          else:
+             print(f'{i}{j}',end=', ')
+       else:
+           continue
+   
