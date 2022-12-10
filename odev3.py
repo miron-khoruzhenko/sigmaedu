1b44cba4f1a37403e4a2b667237fb5decc37ac7b
cank641 <kayalican641@gmail.com>
2022-12-10 21:16:23 +0300

    efesmalt

--- odev3.py
+++ odev3.py
@@ -1,1 +1,16 @@
-open("data.txt","w")
+import time
+
+f = open("data.txt","w")
+f.writelines(["\nName: Al", "\nSurname: Pacino", "\nGender: Male", "\nUsername: Tony Montana", "\nJob: dirty business"])
+f.close()
+time.sleep(1)
+
+a = open("data.txt","r")
+liste = (a.read().split("\n"))
+liste.pop(0)
+dictinoray = {}
+for i in liste:
+    b = i.split(": ")
+    dictinoray.update({b[0]: b[1]})
+
+print(dictinoray)
